2876842203d6d476fa1aff520cbd90abc6640b47
Linton Ye <linton@jimulabs.com>
2014-02-04 22:38:53 -0800

    "add_alarm" animation

--- TimelyMock/mirror/js/add_alarm.js
+++ TimelyMock/mirror/js/add_alarm.js
@@ -1,26 +1,40 @@
 var timeBar = $('@id/timeBar')
 
-var oldX = timeBar.x
+var timeText = $('@id/time', timeBar)
+var oldX = timeText.x
+
+function getNewX(touchX) {
+    var oneThird = timeBar.width/3 - timeText.width
+    var twoThird = timeBar.width/3 * 2
+    var touchedLeft = touchX < timeText.x + timeText.width/2
+    return touchedLeft ? twoThird : oneThird
+}
+
+var lastY = null
 
 timeBar.on('touch',
     function(view, event) {
-        var isUp = event.type == 'up';
-        var newX = timeBar.width - event.x;
-        var toX = isUp ? oldX : newX;
-        var toScale = isUp ? 1 : 0.8;
-        view.$('@id/time').animate({
-            properties: {
-                x: toX,
-                scale: toScale
-            },
-            interpolator: '@android:interpolator/bounce',
-            duration: 100
-        });
+        if (event.type == 'move') {
+            var deltaY = lastY!=null ? event.y-lastY : 0
+            deltaY = Math.min(50, Math.max(-50, deltaY))
+            //log("deltaY="+deltaY+' event.y='+event.y+' lastY='+lastY)
+            lastY = event.y
+            timeBar.y += deltaY
+        } else {
+            var isUp = event.type == 'up';
+            var newX = getNewX(event.x)
+            var toX = isUp ? oldX : newX;
+            var toScale = isUp ? 1 : 0.8;
+            timeText.animate({
+                properties: {
+                    x: toX,
+                    scale: toScale
+                },
+                interpolator: '@android:interpolator/decelerate_cubic',
+                duration: 250
+            });
+            if (isUp) timeBar.animate('@animator/bounce_y')
+            if (event.type=='down') lastY = null
+        }
     });
 
-/*
-timeBar.on('drag',
-    function(view, event) {
-        view.y = event.y
-    });
-*/
