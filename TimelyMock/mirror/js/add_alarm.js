f05ffe1448b6d5639a54028e8d49d8328f456acb
Linton Ye <linton@jimulabs.com>
2014-02-08 08:59:29 -0800

    Better impl with a setTime function

--- TimelyMock/mirror/js/add_alarm.js
+++ TimelyMock/mirror/js/add_alarm.js
@@ -27,33 +27,55 @@ function calcTimeTextSideX(touchX) {
 var timeBarYOffset = 0
 var timeBarParent = timeBar.parent
 var timeBarYMargin = 100
+var currentMinutes = 0
+
+function setTime(ratio, stepInMinute) {
+    ratio = Math.max(0, Math.min(1, ratio))
+    timeBar.y = ratio * (timeBarParent.height - timeBarYMargin - timeBar.height) + timeBar.height
+    var hm = ratio * 24
+    var h = Math.floor(hm)
+    var m = Math.round((hm - h)*60)
+    if (stepInMinute) m = Math.floor(m/stepInMinute)*stepInMinute
+    var zeropadding = function(x) { return x<10 ? '0'+x : x }
+    timeText.text = zeropadding(h) + ':' + zeropadding(m)
+    currentMinutes = h*60 + m
+}
+
+function setTimeInMinutes(minutes) {
+    setTime(minutes / (24*60))
+}
+
+setTimeInMinutes(10*60+30)
+
+var hitTimeBarWhenDown = false
 timeBarParent.on('touch',
     function(view, event) {
-        if (event.type == 'move') {
-            timeBar.y = Math.max(event.y - timeBarYOffset, timeBarYMargin)
-            var hourMinute = event.y * 24 / (timeBarParent.height - timeBar.height)
-            var hour = Math.min(24, Math.max(Math.floor(hourMinute), 0))
-            var minute = hourMinute - hour>0.5 ? ':30' : ':00'
-            timeText.text = hour + minute
+        if (event.type == 'move' && hitTimeBarWhenDown) {
+            var ratio = (event.y- timeBarYMargin) / (timeBarParent.height - timeBarYMargin)
+            setTime(ratio, 30)
         } else {
-            var isUp = event.type == 'up';
-            var sideX = calcTimeTextSideX(event.x)
-            var centerX = (timeBar.width - timeText.width)/2
-            var toX = isUp ? centerX : sideX;
-            var toScale = isUp ? 1 : 0.8;
-            timeText.animate({
-                properties: {
-                    x: toX,
-                    scale: toScale
-                },
-                interpolator: '@android:interpolator/decelerate_cubic',
-                duration: 250
-            });
-            if (isUp) {
-                timeBar.animate('@animator/bounce_y')
-                playRipple(event.x + view.x, event.y + view.y)
+            var isUp = event.type == 'up'
+            var isDown = event.type == 'down'
+            var hitTimeBar = timeBar.y<=event.y && event.y<=timeBar.y+timeBar.height
+            if (isDown) hitTimeBarWhenDown = hitTimeBar
+
+            if (hitTimeBarWhenDown) {
+                var sideX = calcTimeTextSideX(event.x)
+                var centerX = (timeBar.width - timeText.width)/2
+                timeText.animate({
+                    properties: {
+                        x: isUp ? centerX : sideX,
+                        scale: isUp ? 1 : 0.8
+                    },
+                    interpolator: '@android:interpolator/decelerate_cubic',
+                    duration: 250
+                });
+                if (isUp) timeBar.animate('@animator/bounce_y')
+            } else if (isUp) {
+                var sign = event.y < timeBar.y ? -1 : 1
+                setTimeInMinutes(currentMinutes + sign*5)
             }
-            if (!isUp) timeBarYOffset = event.y - timeBar.y
+            if (isUp) playRipple(event.x + view.x, event.y + view.y)
         }
     }
 );
