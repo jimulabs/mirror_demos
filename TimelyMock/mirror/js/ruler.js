35e6984b5c88c552220e0acf22fb12b1f2df195b
Linton Ye <linton@jimulabs.com>
2014-02-11 12:19:40 -0800

    Name variable

--- TimelyMock/mirror/js/ruler.js
+++ TimelyMock/mirror/js/ruler.js
@@ -22,14 +22,14 @@ function magnifyRuler(ratio) {
     var ruler = $('@id/ruler')
     var children = ruler.children, childCount = children.size()
     var center = Math.floor(childCount * ratio)
-    var radius = 4
+    var radius = 4, maxScale = 4
     var lo = Math.max(0, center-radius), hi = Math.min(childCount-1, center+radius)
     for (i=0; i<childCount; i++) {
         var v = children.get(i)
         if (lo<=i && i<=hi) {
             var vVsCenter = 1 - Math.abs(i/childCount - ratio)*5
 //            log('i='+i+' vVsCenter='+vVsCenter+' ratio='+ratio+' childCount='+childCount)
-            setRulerLabelParams(v, vVsCenter, 4 * vVsCenter)
+            setRulerLabelParams(v, vVsCenter, maxScale * vVsCenter)
         } else {
             showNormalLabelOnRuler(v, i)
         }
