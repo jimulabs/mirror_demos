2a4f13f799b83d2e932e401a5298395507ee2984
Linton Ye <linton@jimulabs.com>
2014-02-15 14:52:29 -0800

    Add end-branch param to cherry.rb

--- cherry.rb
+++ cherry.rb
@@ -1,16 +1,17 @@
 def cmd(command)
+  puts command
   puts `#{command}`
 end
 
 args = ARGV
-if args.size!=2
-  puts "Usage: ruby cherry.rb <commit> [start-branch]"
+if args.size<2
+  puts "Usage: ruby cherry.rb <commit> <start-branch> [<end-branch>]"
 else
   commit = ARGV[0]
   MIN_BRANCH = 0
   MAX_BRANCH = 8
   start_branch = ARGV[1].to_i
-  end_branch = MAX_BRANCH
+  end_branch = ARGV[2]==nil ?  MAX_BRANCH : ARGV[2].to_i
   if start_branch<MIN_BRANCH or start_branch>MAX_BRANCH
     puts "invalid start branch:#{start_branch}, should be between #{MIN_BRANCH} and #{MAX_BRANCH}"
   else
